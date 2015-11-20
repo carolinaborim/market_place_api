8e742dd53dc9ed5bb874e073cbbdc235364c3b12
Carolina Borim <cborim@thoughtworks.com>
2015-11-20 16:06:36 -0200

    adding first spec and making tests pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
-Rails.application.routes.draw do
-  namespace :api, defaults: {format: :json}, constraints: { subdomain: 'api' }, path: '/' do
-    # resources come here
+require 'api_constraints'
+
+MarketPlaceApi::Application.routes.draw do
+  # Api definition
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/'  do
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
+      
+    end
   end
 end
